00d105b8b8506ac69add9e50e69c8adc06790bdd
xtturing <xtturing@gmail.com>
2014-07-27 22:27:10 +0800

    Initial Commit

---
+++ tdtnb/tdtnb/NBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NBAppDelegate.h
+//  tdtnb
+//
+//  Created by xtturing on 14-7-27.
+//  Copyright (c) 2014年 xtturing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tdtnb/tdtnb/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tdtnb
+//
+//  Created by xtturing on 14-7-27.
+//  Copyright (c) 2014年 xtturing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NBAppDelegate class]));
+    }
+}
